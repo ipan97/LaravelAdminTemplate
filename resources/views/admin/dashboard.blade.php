c8f24475badbd8949f56c9a1b9b539867d344b91
Ipan Taupik Rahman <ipantaupik97@gmail.com>
2018-03-01 17:24:44 +0700

    refactor layout yield content

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,17 +1,31 @@
 @extends('admin.layouts.admin')
 
 @section('content')
-    <div class="row">
-        <div class="col-xs-12 col-md-12 col-sm-12">
-            <div class="box box-transparent">
-                <div class="box-title">
-                    <h1>Dashboard</h1>
-                </div>
-                <div class="box-body">
+    <div class="content-wrapper">
+        <section class="content-header">
+            <h1>
+                Page Header
+                <small>Optional description</small>
+            </h1>
+            <ol class="breadcrumb">
+                <li><a href="#"><i class="fa fa-dashboard"></i> Level</a></li>
+                <li class="active">Here</li>
+            </ol>
+        </section>
+        <section class="content">
+            <div class="row">
+                <div class="col-md-3 col-sm-6 col-xs-12">
+                    <div class="info-box">
+                        <span class="info-box-icon bg-yellow"><i class="ion ion-ios-people-outline"></i></span>
 
+                        <div class="info-box-content">
+                            <span class="info-box-text">New Members</span>
+                            <span class="info-box-number">2,000</span>
+                        </div>
+                    </div>
                 </div>
             </div>
-        </div>
+        </section>
     </div>
 @endsection()
 
--- resources/views/admin/layouts/admin.blade.php
+++ resources/views/admin/layouts/admin.blade.php
@@ -29,30 +29,9 @@
 </head>
 <body class="hold-transition skin-blue sidebar-mini">
 <div class="wrapper">
-@include('admin.layouts.partials.header')
-@include('admin.layouts.partials.sidebar')
-
-<!-- Content Wrapper. Contains page content -->
-    <div class="content-wrapper">
-        <!-- Content Header (Page header) -->
-        <section class="content-header">
-            <h1>
-                Page Header
-                <small>Optional description</small>
-            </h1>
-            <ol class="breadcrumb">
-                <li><a href="#"><i class="fa fa-dashboard"></i> Level</a></li>
-                <li class="active">Here</li>
-            </ol>
-        </section>
-
-        <!-- Main content -->
-        <section class="content container-fluid">
-            @yield('content')
-        </section>
-        <!-- /.content -->
-    </div>
-    <!-- /.content-wrapper -->
+    @include('admin.layouts.partials.header')
+    @include('admin.layouts.partials.sidebar')
+    @yield('content')
     @include('admin.layouts.partials.footer')
 </div>
 <!-- ./wrapper -->
